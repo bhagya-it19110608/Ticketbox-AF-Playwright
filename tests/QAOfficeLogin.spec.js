5b4027b56a71fdef2fde416abd9682c13c15e58d
bhagya-it19110608 <bhagyaweerasinghe02@gmail.com>
2026-05-28 16:13:45 +0530

    Removed only stack from test

--- tests/QAOfficeLogin.spec.js
+++ tests/QAOfficeLogin.spec.js
@@ -1,6 +1,6 @@
 const {test, expect} = require('@playwright/test');
 
-test.only('Ticketbox QA Office Load - Login failed (Invalid email)', async ({browser})=>
+test('Ticketbox QA Office Load - Login failed (Invalid email)', async ({browser})=>
 {
     //chrome plugins
     const context = await browser.newContext();
