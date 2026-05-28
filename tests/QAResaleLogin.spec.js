8499673e1f3f89c8330892c531f77ccd400306d0
bhagya-it19110608 <bhagyaweerasinghe02@gmail.com>
2026-05-28 16:13:01 +0530

    Resale page load verified

--- tests/QAResaleLogin.spec.js
+++ tests/QAResaleLogin.spec.js
@@ -1,14 +1,15 @@
 const {test, expect} = require('@playwright/test');
 
-test('Ticketbox QA Resale  Load', async ({page})=>
+test.only('Ticketbox QA Resale page load)', async ({browser})=>
 {
-    await page.goto("https://qa-resale.ticketbox.lk");
-
+    //chrome plugins
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://qa-resale.ticketbox.lk/");
+    
     //get title - assertion
    console.log (await page.title());
    await expect(page).toHaveTitle("TicketBox Reselling - Sri Lanka's First and Premium Event Tickets Reselling Platform");
-
-   await page.locator('#email').fill('ticketboxlk123@gmail.com');
-   await page.locator("[placeholder='Enter your password']").fill('Pass123$');
-   await page.locator("[type='submit']").click();
 });
+
+
