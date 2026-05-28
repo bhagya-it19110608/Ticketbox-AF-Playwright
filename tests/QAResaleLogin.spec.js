01ec76c2a360de0957c1d537aa45561feb665c53
bhagya-it19110608 <bhagyaweerasinghe02@gmail.com>
2026-05-28 16:39:24 +0530

    Cookies accepted

--- tests/QAResaleLogin.spec.js
+++ tests/QAResaleLogin.spec.js
@@ -6,10 +6,18 @@ test.only('Ticketbox QA Resale page load)', async ({browser})=>
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://qa-resale.ticketbox.lk/");
-    
+
+    const accept = page.locator("//button[normalize-space()='Accept All']");
+
     //get title - assertion
    console.log (await page.title());
    await expect(page).toHaveTitle("TicketBox Reselling - Sri Lanka's First and Premium Event Tickets Reselling Platform");
+
+   //Cookie Acceptance
+   await accept.click();
+   
+
+
 });
 
 
