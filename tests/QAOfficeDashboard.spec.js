866625ee04cb129b8f1b703f28770b74a6dc3d5e
bhagya-it19110608 <bhagyaweerasinghe02@gmail.com>
2026-06-18 12:52:02 +0530

    Dashboard title and description captured

--- tests/QAOfficeDashboard.spec.js
+++ tests/QAOfficeDashboard.spec.js
@@ -10,8 +10,9 @@ test.only('Ticketbox QA Office Load', async ({browser})=>
     const email = page.locator('#email');
     const password = page.locator("[placeholder='Enter your password']");
     const signin = page.locator("[type='submit']");
-    const notification = page.locator("[aria-live='assertive']");
     const welcome = page.locator("[aria-live='off']");
+    const dashboard = page.locator("//h2[text()='Dashboard']");
+    const description = page.locator("//p[text()='Welcome to your TicketBox organizer panel']");
     
     //get title - assertion
    console.log (await page.title());
@@ -21,8 +22,12 @@ test.only('Ticketbox QA Office Load', async ({browser})=>
    await email.fill('ticketboxlk123@gmail.com');
    await password.fill('Pass123$');
    await signin.click();
-   await expect (page).toHaveURL('https://qa-office\.ticketbox\.lk/');
+   await expect (page).toHaveURL('https://qa-office\.ticketbox\.lk/login');
    console.log(await welcome.textContent());
    await expect(welcome).toContainText('Login Successful');
+   console.log(await dashboard.textContent());
+   await expect(dashboard).toContainText('Dashboard');
+   console.log(await description.textContent());
+   await expect(description).toContainText('Welcome to your TicketBox organizer panel');
 });
 
